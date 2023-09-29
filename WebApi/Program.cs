f9fc26816763991c5c2fdcbdff69f60f0c3d3123
Aleksandr <sanya.romaniuta@gmail.com>
2023-09-29 09:47:09 +0300

    да просто создал апи проект

---
+++ WebApi/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
+namespace WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
