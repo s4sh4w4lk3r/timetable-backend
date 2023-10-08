695d1dcb5a268a42be9abaae1a1fb093b3e175ed
Aleksandr <sanya.romaniuta@gmail.com>
2023-10-08 17:24:46 +0300

    добавил обобщенный репозиторий

---
+++ src/Repository/Implementations/EFCore/EFCoreRepository.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Repository.Implementations.MySql;
+using Repository.Interfaces;
+
+namespace Repository.Implementations.EFCore
+{
+    public class EFCoreRepository<T> : IRepository<T> where T : class
+    {
+        private MySqlDbContext _context;
+        private DbSet<T> _dbSet;
+        private CancellationToken _cancellationToken;
+        private IValidator<T> _validator;
+
+        public EFCoreRepository(MySqlDbContext context, IValidator<T> validator, CancellationToken cancellationToken = default)
+        {
+            _context = context;
+            _cancellationToken = cancellationToken;
+            _validator = validator;
+            _dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> Entites => _dbSet.AsQueryable();
+
+        public async Task DeleteAsync(T entity)
+        {
+            _validator.ValidateAndThrow(entity);
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync(_cancellationToken);
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            _validator.ValidateAndThrow(entity);
+
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync(_cancellationToken);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _validator.ValidateAndThrow(entity);
+
+            _dbSet.Update(entity);
+
+            await _context.SaveChangesAsync(_cancellationToken);
+        }
+    }
+}
---
+++ src/Repository/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Repository.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> Entites { get; }
+        Task InsertAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+    }
+}
