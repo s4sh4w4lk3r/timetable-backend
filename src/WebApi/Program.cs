8020d0eb7a7d87d0c1d79b78b1950220984435b1
Aleksandr <sanya.romaniuta@gmail.com>
2023-10-14 00:04:53 +0300

    в program добавил логгирование в файл, а также валидаторы в IoC

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,4 +1,12 @@
+using FluentValidation;
 using Microsoft.AspNetCore.HttpOverrides;
+using Models.Validation;
+using Repository;
+using Repository.Implementations.EFCore;
+using Repository.Interfaces;
+using Serilog;
+using WebApi.ConfigurationTypes;
+using WebApi.ConfigurationTypes.Validation;
 
 namespace WebApi
 {
@@ -8,11 +16,29 @@ namespace WebApi
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console()
+            .WriteTo.File($"./logs/log.log", rollingInterval: RollingInterval.Day)
+            .ReadFrom.Configuration(ctx.Configuration));
+
             builder.Services.Configure<DbConfiguration>(builder.Configuration.GetRequiredSection(nameof(DbConfiguration)));
             builder.Services.Configure<JwtConfiguration>(builder.Configuration.GetRequiredSection(nameof(JwtConfiguration)));
 
             builder.Services.AddScoped(typeof(IRepository<>), typeof(EFCoreRepository<>));
+
+            builder.Services.AddValidatorsFromAssemblyContaining<ApprovalCodeValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<CabinetValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<GroupValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<LessonTimeValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<SubjectValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<TeacherValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<TimetableCellValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<TimetableValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<UserValidator>();
+            builder.Services.AddValidatorsFromAssemblyContaining<JwtConfigurationValidator>();
+
             var app = builder.Build();
+
+            app.UseSerilogRequestLogging();
 
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
