bf435960320b0a10536b87799d74be5c2d2536f7
Aleksandr <sanya.romaniuta@gmail.com>
2023-10-21 11:53:06 +0300

    прокачал serviceresult, добавил статик методы и tostring оверрайды

--- src/WebApi/Services/ServiceResult.cs
+++ src/WebApi/Services/ServiceResult.cs
@@ -14,6 +14,31 @@ public class ServiceResult
     {
         InnerServiceResult = innerServiceResult;
     }
+
+    #region Статические методы.
+    public static ServiceResult Fail(string description)
+    {
+        return new ServiceResult(false, description);
+    }
+    public static ServiceResult Ok(string description)
+    {
+        return new ServiceResult(true, description);
+    }
+
+    public static ServiceResult Fail(string description, ServiceResult innerServiceResult)
+    {
+        return new ServiceResult(false, description, innerServiceResult);
+    }
+    public static ServiceResult Ok(string description, ServiceResult innerServiceResult)
+    {
+        return new ServiceResult(true, description, innerServiceResult);
+    }
+    #endregion
+
+    public override string ToString()
+    {
+        return $"Success: {Success}, Description: {Description}, InnverServiceResult: {InnerServiceResult}";
+    }
 }
 public class ServiceResult<T> : ServiceResult
 {
@@ -26,5 +51,30 @@ public class ServiceResult<T> : ServiceResult
     public ServiceResult(bool success, string description, ServiceResult innerServiceResult, T? value) : base(success, description, innerServiceResult)
     {
         Value = value;
+    }
+
+    #region Статические методы.
+    public static ServiceResult<T> Fail(string description, T? value)
+    {
+        return new ServiceResult<T>(false, description, value);
+    }
+    public static ServiceResult<T> Ok(string description, T? value)
+    {
+        return new ServiceResult<T>(true, description, value);
+    }
+
+    public static ServiceResult<T> Fail(string description, ServiceResult innerServiceResult, T? value)
+    {
+        return new ServiceResult<T>(false, description, innerServiceResult, value);
+    }
+    public static ServiceResult<T> Ok(string description, ServiceResult innerServiceResult, T? value)
+    {
+        return new ServiceResult<T>(true, description, innerServiceResult, value);
+    }
+    #endregion
+
+    public override string ToString()
+    {
+        return $"{base.ToString()}, Value: {Value}";
     }
 }
