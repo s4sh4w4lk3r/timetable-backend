73a7bb0a217c1543dccfa35a3733f9a0667bdaa2
Aleksandr <sanya.romaniuta@gmail.com>
2023-11-19 20:36:15 +0300

    убрал лишнее из сваггер док

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -53,19 +53,8 @@ public class Program
             options.SwaggerDoc("v1", new OpenApiInfo
             {
                 Version = "v1",
-                Title = "ToDo API",
-                Description = "An ASP.NET Core Web API for managing ToDo items",
-                TermsOfService = new Uri("https://example.com/terms"),
-                Contact = new OpenApiContact
-                {
-                    Name = "Example Contact",
-                    Url = new Uri("https://example.com/contact")
-                },
-                License = new OpenApiLicense
-                {
-                    Name = "Example License",
-                    Url = new Uri("https://example.com/license")
-                }
+                Title = "Timetable WebApi",
+                Description = "Timetable WebApi"
             });
 
             var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
